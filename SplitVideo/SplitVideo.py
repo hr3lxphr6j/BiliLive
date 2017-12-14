07716b597d0b11c895f0c7e5c1257ce71be8b4fc
chigusa <c02110210610204@gmail.com>
2017-12-15 06:16:39 +0800

    -- 逻辑优化，添加注释

--- SplitVideo/SplitVideo.py
+++ SplitVideo/SplitVideo.py
@@ -1,69 +1,115 @@
 import os
+import re
 import sys
 import json
 import math
 import yaml
 import logging
 import datetime
+import functools
 import subprocess
 
 from zlib import crc32
 from hashlib import sha1, md5
-from multiprocessing.dummy import Pool as ThreadPool
+from multiprocessing.dummy import Pool
 
 logging.basicConfig(format='[%(asctime)s] [%(levelname)s] %(message)s', level=logging.INFO)
 
 
-def parse_config(filename):
-    try:
-        with open(filename, encoding='utf8') as f:
-            return yaml.load(f)
-    except Exception as e:
-        logging.error(e)
-        exit(1)
+def parse_config(filename: str) -> dict:
+    """
+    解析YAML
+    :param filename: yaml文件路径
+    :return: dict
+    """
+    with open(filename, encoding='utf8') as f:
+        return yaml.load(f)
 
 
-def file_list_to_byte(path, file_list):
+def file_list_to_byte(path: str, file_list: list) -> bytes:
+    """
+    将文件列表转为ffmpeg的Virtual Concatenation Script(https://www.ffmpeg.org/ffmpeg-all.html#concat-1)
+    :param path: 文件所在路径
+    :param file_list: 文件集合
+    :return: Virtual Concatenation Script
+    """
     b = b''
     for f in file_list:
         b += ("file '%s'\n" % (os.path.join(path, f))).encode()
     return b
 
 
-def get_sha1_hex(filename):
+def get_sha1_hex(filename: str, egg_pain: bool = False) -> bytes:
+    """
+    获取文件（名）的sha1值
+    :param filename: 文件url
+    :param egg_pain: 使用文件的SHA1值，而不是文件名的(神经病)
+    :return: SHA1
+    """
     sha1_obj = sha1()
-    sha1_obj.update(filename.encode())
-    # with open(filename, 'rb') as f:
-    #     for l in f:
-    #         sha1_obj.update(l)
+    if egg_pain:
+        with open(filename, 'rb') as f:
+            for l in f:
+                sha1_obj.update(l)
+    else:
+        sha1_obj.update(filename.encode())
     return sha1_obj.digest()
 
 
-def get_crc32(filename):
+def get_crc32(filename: str, egg_pain: bool = False) -> int:
+    """
+    获取文件（名）的CRC32值
+    :param filename: 文件url
+    :param egg_pain: 使用文件的CRC32值，而不是文件名的(神经病)
+    :return: CRC32
+    """
     crc = 0
-    with open(filename, 'rb') as f:
-        for l in f:
-            crc = crc32(l, crc)
+    if egg_pain:
+        with open(filename, 'rb') as f:
+            for l in f:
+                crc = crc32(l, crc)
+    else:
+        crc = crc32(filename.encode())
     return crc
 
 
-def get_remux_file_name(path, file_list):
+def get_file_list_md5(path: str, file_list: list) -> str:
+    """
+    根据文件集合计算md5，用于为重封装的文件命名
+    计算方式：md5( sha1(文件01) + sha1(文件02) + ... + sha1(文件n) )
+    :param path: 文件所在路径
+    :param file_list: 文件集合
+    :return: md5 string
+    """
     sha1s = b''
     for f in file_list:
         sha1s += get_sha1_hex(os.path.join(path, f))
     return md5(sha1s).hexdigest()
 
 
-def is_remuxed_file_exist(path, file_list):
-    md5_name = get_remux_file_name(path, file_list)
+def is_remuxed_file_exist(path: str, file_list: list) -> str:
+    """
+    查找已存在的重封装文件，如果存在，返回路径
+    :param path: 文件所在路径
+    :param file_list: 文件集合
+    :return: 重封装文件url
+    """
+    md5_name = get_file_list_md5(path, file_list)
     for rt, dirs, files in os.walk(path):
         for file in files:
-            if md5_name in file:
+            if re.match('%s\[\w{8}\]\.mp4' % md5_name, file):
                 return os.path.join(path, file)
 
 
-def merge_video_to_file(path, file_list):
-    md5_name = get_remux_file_name(path, file_list)
+def merge_video_to_file(path: str, file_list: list) -> str:
+    """
+    重封装视频
+    重封装结束后，会在文件名后添加一个CRC32字符串，用来标记其完整性
+    :param path: 文件所在路径
+    :param file_list: 文件集合
+    :return: 重封装文件url
+    """
+    md5_name = get_file_list_md5(path, file_list)
     remux_file_name = os.path.join(path, md5_name + '.mp4')
     child = subprocess.Popen(
         ['ffmpeg',
@@ -84,12 +130,22 @@ def merge_video_to_file(path, file_list):
     return res_file_name
 
 
-def str2time(s):
+def str2time(s: str) -> datetime.timedelta:
+    """
+    将字符串表示的时间转为timedelta对象
+    :param s: 字符串表示的时间
+    :return: timedelta obj
+    """
     t = s.split(':')
     return datetime.timedelta(hours=int(t[0]), minutes=int(t[1]), seconds=int(t[2]))
 
 
-def get_video_duration(filename):
+def get_video_duration(filename: str) -> datetime.timedelta:
+    """
+    获取视频长度
+    :param filename: 文件url
+    :return: timedelta obj
+    """
     child = subprocess.Popen(
         ['ffprobe',
          '-loglevel', '-8',
@@ -102,8 +158,29 @@ def get_video_duration(filename):
     return datetime.timedelta(seconds=float(json.loads(out)['format']['duration']))
 
 
-def cut_video(input_file, output_file, start_time, end_time, is_concat=True):
-    logging.info('Splitting To: %s' % output_file)
+def log(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kw):
+        logging.info('Splitting Video To\t%s' % args[1] or kw['output_file'])
+        t = datetime.datetime.now()
+        func(*args, **kw)
+        logging.info(
+            'Split To\t%s\tTime Spent\t%s' % (args[1] or kw['output_file'], str(datetime.datetime.now() - t)))
+
+    return wrapper
+
+
+@log
+def cut_video(input_file: str, output_file: str, start_time: str, end_time: str, is_concat: bool = True):
+    """
+    分割视频
+    :param input_file: 输入文件
+    :param output_file: 输出文件
+    :param start_time: 起始时间
+    :param end_time: 结束时间
+    :param is_concat: 输入是否为Virtual Concatenation Script
+    :return:
+    """
     if is_concat:
         child = subprocess.Popen(
             ['ffmpeg',
@@ -134,10 +211,17 @@ def cut_video(input_file, output_file, start_time, end_time, is_concat=True):
             stderr=subprocess.PIPE
         )
         child.communicate()
-    logging.info('Split Completed: %s' % output_file)
 
 
-def get_abs_time(time, part, time_line, is_end=False):
+def get_abs_time(time: datetime.timedelta, part: int, time_line: list, is_end: bool = False) -> datetime.timedelta:
+    """
+    将相对于每个文件的时间点转化为相对于所有文件合并后总时长中对应的时间点
+    :param time: 输入时间
+    :param part: 所处文件
+    :param time_line: 时间线
+    :param is_end: 是否为结尾时间
+    :return: timedelta obj
+    """
     if not -1 <= part < len(time_line) - 1:
         logging.error('Part Index: %d Was Out Of Range!' % part)
         exit(1)
@@ -153,13 +237,20 @@ def get_abs_time(time, part, time_line, is_end=False):
             return min(time + time_line[part], time_line[part + 1])
 
 
-def limit_part_length(storyboard, part_time, greedy_percentage):
+def limit_part_length(storyboard: list, part_time: datetime.timedelta, greedy_percentage: float) -> list:
+    """
+    限制每一部分时长不超过规定值
+    :param storyboard: 故事板
+    :param part_time: 每段的最大时间长度
+    :param greedy_percentage: 视频长度对part_time取余数(最后1Part长度)，如果时长不超过part_time * greedy_percentage则不独立分段
+    :return: 新的故事板
+    """
     new_storyboard = []
     for part in storyboard:
         if part['duration'] >= (part_time * (1 + greedy_percentage)):
             duration = part['duration']
             new_part = []
-            for i in range(int(math.ceil(part['duration'] / part_time))):
+            for i in range(int(math.ceil(duration / part_time))):
                 start = new_part[-1]['end'] if len(new_part) != 0 else part['start']
                 dur = min(duration, part_time)
                 duration -= dur
@@ -181,27 +272,76 @@ def limit_part_length(storyboard, part_time, greedy_percentage):
 
 
 def main():
+    global config, global_config, projects
+
     if len(sys.argv[1:]) == 0:
         logging.error('One YAML Config File Should Be Input!')
-    config = parse_config(sys.argv[1])
-    thread_num = config['GlobalConfig']['ProcessThread']
-    for proj in config['Projects']:
-        logging.info('Start Process Project %d' % (config['Projects'].index(proj) + 1))
-        path = proj['Path']
-        file_list = proj['Files']
-        parts = proj['Parts']
-        video_durations = []
+
+    config_file = sys.argv[1]
+
+    try:
+        config = parse_config(config_file)
+    except Exception:
+        logging.error('Can Not Parse Config File: %s' % config_file)
+        exit(1)
+
+    try:
+        global_config = config['GlobalConfig']
+        projects = config['Projects']
+    except KeyError as e:
+        logging.error('Config File Error, "%s" Node Did Not Exist!' % e)
+
+    # 是否限制Part长度
+    is_limit_part_length = global_config.get('LimitPartLength', True)
+    # 每段视频最大长度
+    part_time = str2time(global_config.get('PartTime', '1:00:00'))
+    # 视频长度对part_time取余数(最后1Part长度)，如果时长不超过part_time * greedy_percentage则不独立分段
+    greedy_percentage = global_config.get('GreedyPercentage', 0.2)
+    # 是否先重封装为Mp4，提供索引加快分段速度
+    is_remux = global_config.get('RemuxToMp4', False)
+    # 并行处理数量(线程池大小)
+    thread_num = global_config.get('ProcessThread', os.cpu_count() or 1)
+
+    # 打印全局配置信息
+    logging.info('+' * 40)
+    logging.info('Global Config Info:')
+    logging.info('LimitPartLength: %s' % is_limit_part_length)
+    if is_limit_part_length:
+        logging.info('PartTime: %s' % str(part_time))
+        logging.info('GreedyPercentage: %0.02f' % greedy_percentage)
+    logging.info('RemuxToMp4: %s' % is_remux)
+    logging.info('ProcessThread: %d' % thread_num)
+    logging.info('+' * 40 + '\n')
+
+    # 初始化线程池
+    pool = Pool(thread_num)
+    # 记录重封装文件
+    remux_file_names = []
+
+    for project in projects:
+
+        logging.info('Start Process Project %d' % (config['Projects'].index(project) + 1))
+
+        # 文件所在路径
+        path = project['Path']
+        # 视频文件集合
+        file_list = project['Files']
+        # 分段集合
+        parts = project['Parts']
+
+        # 记录每个视频文件在总长度的起始位置，末尾为总视频长度
         time_line = [datetime.timedelta()]
-        total_video_duration = datetime.timedelta()
         for file in file_list:
             duration = get_video_duration(os.path.join(path, file))
-            logging.info('File: %s, Duration: %s' % (file, str(duration)))
-            video_durations.append(duration)
             time_line.append(time_line[-1] + duration)
-            total_video_duration += duration
-        logging.info('Total Video Duration: %s' % str(total_video_duration))
+            logging.info('File: %s, Duration: %s' % (file, str(duration)))
+        logging.info('Total Video Duration: %s' % str(time_line[-1]))
+
+        # 重封装文件名
         remux_file_name = None
-        if config['GlobalConfig']['RemuxToMp4']:
+
+        # 处理重封装逻辑
+        if is_remux:
             remux_file_name = is_remuxed_file_exist(path, file_list)
             if not remux_file_name:
                 logging.info('No Remuxed File Found, Begin To Remux...')
@@ -212,10 +352,15 @@ def main():
                     exit(0)
             else:
                 logging.info('Remuxed File Found, File Name: %s' % remux_file_name)
+            remux_file_names.append(remux_file_name)
+
+        # 故事板，记录经过解析的分段信息
+        storyboard = []
+
+        # 解析分段信息
         if len(parts) == 0:
             logging.error('No Part Info Defined!')
             exit(1)
-        storyboard = []
         for part in parts:
             start = get_abs_time(str2time(part['StartTime'][0]), part['StartTime'][1], time_line)
             end = get_abs_time(str2time(part['EndTime'][0]), part['EndTime'][1], time_line, True)
@@ -228,32 +373,40 @@ def main():
                 'end': end,
                 'duration': end - start
             })
-        if config['GlobalConfig']['LimitPartLength']:
-            storyboard = limit_part_length(storyboard, str2time(config['GlobalConfig']['PartTime']),
-                                           config['GlobalConfig']['GreedyPercentage'])
-        logging.info('Part Info:')
+
+        # 处理分段长度限制逻辑
+        if is_limit_part_length:
+            storyboard = limit_part_length(storyboard, part_time, greedy_percentage)
+
+        # 打印分段信息
+        logging.info('+' * 40)
+        logging.info('Project No: %02d, Part Info:' % (projects.index(project) + 1))
         for part in storyboard:
-            logging.info('=' * 40)
-            logging.info('Part No: %d' % (storyboard.index(part) + 1))
-            logging.info('PartName: %s' % part['name'])
-            logging.info('StartTime: %s' % str(part['start']))
-            logging.info('EndTime: %s' % str(part['end']))
-            logging.info('Duration: %s' % str(part['duration']))
-            logging.info('=' * 40)
-        logging.info('Begin To Split Video, Process Thread Num: %d' % thread_num)
-        pool = ThreadPool(thread_num)
+            logging.info('Part No\t%02d' % (storyboard.index(part) + 1))
+            logging.info('PartName\t%s' % part['name'])
+            logging.info('StartTime\t%s' % str(part['start']))
+            logging.info('EndTime\t%s' % str(part['end']))
+            logging.info('Duration\t%s' % str(part['duration']))
+            if part != storyboard[-1]:
+                logging.info('=' * 35)
+        logging.info('+' * 40)
+
+        # 向线程池中异步提交任务
         for part in storyboard:
             pool.apply_async(cut_video, (
-                remux_file_name if config['GlobalConfig']['RemuxToMp4'] else file_list_to_byte(path, file_list),
+                remux_file_name if is_remux else file_list_to_byte(path, file_list),
                 os.path.join(path, '%02d.%s.mp4' % (storyboard.index(part) + 1, part['name'])),
                 str(part['start']),
                 str(part['end']),
-                not config['GlobalConfig']['RemuxToMp4']
+                not is_remux
             ))
-        pool.close()
-        pool.join()
-        logging.info('Process Project %d Completed!' % (config['Projects'].index(proj) + 1))
+
+    # 关闭线程池，并等待其中所有任务完成
+    pool.close()
+    pool.join()
     logging.info('All Project Completed!')
+    for f in remux_file_names:
+        os.remove(f)
 
 
 if __name__ == '__main__':
