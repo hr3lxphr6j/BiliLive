e4dc013ddeabda949286a7df841614e93f69d13d
chigusa <c02110210610204@gmail.com>
2017-12-29 19:24:01 +0800

    -- Log使用英文，防止奇怪问题

--- Monitor.py
+++ Monitor.py
@@ -32,20 +32,20 @@ class Monitor:
             self.room = ZhanqiLive(self.room_id)
 
     def run(self):
-        logging_title = u'[Platform:%s Room:%s Up:%s]' % (
+        logging_title = '[Platform:%s Room:%s Up:%s]' % (
             self.site_domain, self.room_id, self.room.get_room_info()['hostname'])
-        self.logger.info(u'%s Start Monitoring' % logging_title)
+        self.logger.info('%s Start Monitoring' % logging_title)
         while 1:
             try:
                 if self.room.get_room_info()['status']:
-                    self.logger.info(u'%s Begin Recoding' % logging_title)
+                    self.logger.info('%s Begin Recoding' % logging_title)
                     time.sleep(self.config['LAZY_TIME'])
-                    self.logger.info(u'%s Start Recoding' % logging_title)
+                    self.logger.info('%s Start Recoding' % logging_title)
                     t = Recoder(self.room.get_live_urls()[0], self.config['OUTPUT_DIR'],
                                 '%s %s.%s' % (utils.get_current_time(), self.room.get_room_info()['roomname'],
                                               self.config['OUTPUT_FILE_EXT'])).start_recoding()
                     if t > 30:
-                        self.logger.info(u'%s Recoded Over!\tTime:%s s' % (logging_title, str(round(t))))
+                        self.logger.info('%s Recoded Over!\tTime:%s s' % (logging_title, str(round(t))))
                 else:
                     time.sleep(self.config['POLLING_INTERVAL'])
             except Exception:
